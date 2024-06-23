3a790772f48774aa71706c12fe25e760aca34f5f
nikhil77gautam <135546239+nikhil77gautam@users.noreply.github.com>
2024-06-23 13:28:11 +0530

    allset

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { BsDownload } from "react-icons/bs";
-import pdf from "../Nikhil_Gautam-CV3.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
+import pdf from "../Nikhil_Gautam-CV3.pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 const Resume = () => {
   const [wid, setwid] = useState(window.innerWidth);
